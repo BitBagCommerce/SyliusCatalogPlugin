8603763456975e7a4f27872bab9d7ab49e564d57
Ala-Troszczynska <ala.troszczynska@o2.pl>
2020-11-09 13:29:39 +0100

    Twig and form

--- src/Form/Type/CatalogType.php
+++ src/Form/Type/CatalogType.php
@@ -12,15 +12,22 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusCatalogPlugin\Form\Type;
 
-use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CatalogType extends AbstractResourceType
+class CatalogType extends AbstractType
 {
-//    public function buildForm(FormBuilderInterface $builder, array $options): void
-//    {
-//        $builder
-//            ->add('name', TextType::class, ['label' => "Name"]);
-//    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => "Name"])
+            ->add( 'submit', SubmitType::class, ['label' => "Create"]);
+    }
+
+
+
 }
